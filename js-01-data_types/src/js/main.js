6972000b3e1688257a585df9a4ab1d2d1e305df8
Antonio Amaro <ja.antonioamaro@gmail.com>
2025-11-26 12:48:00 -0600

    feat: anotaciones

---
+++ js-01-data_types/src/js/main.js
@@ -0,0 +1,15 @@
+// console.log("Desde main")
+
+let fullName = 'Antonio Amaro'
+let age = 28
+let auth = false
+
+const firstName = "Antonio";
+let currentAge = 28;
+const nationality = "Mexican";
+// De las variables anteriores muestra en console el sig. mensaje:
+// "My name is Daniel, I am 24 years old and I am Mexican."
+
+// console.log(`My name is ${firstName}, I am ${currentAge} years old, and I'm ${nationality}`)
+console.log('s' * 's')
+console.log(typeof '4' * '3')
